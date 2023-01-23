70244a3ae4b8fc1eea37d7653748a4af729ecf95
SpringWangLA <developer.xamarin.hoang@gmail.com>
2023-01-23 22:44:03 +0700

    Include Student router to app

--- app/server/app.py
+++ app/server/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from server.routes.student import router as StudentRouter
+
 app = FastAPI()
 
-@app.get('/', tags=["Root"])
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
+
+
+@app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to this first FastAPI App!"}
+    return {"message": "Welcome to this fantastic app!"}
     

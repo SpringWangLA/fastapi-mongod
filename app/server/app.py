4f485c3b97c35124fcbe72a08288b77037463b83
SpringWangLA <developer.xamarin.hoang@gmail.com>
2023-02-01 20:47:35 +0700

    Add functiontest router

--- app/server/app.py
+++ app/server/app.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 from server.routes.student import router as StudentRouter
 
+from server.routes.functiontest import router as FunctionTestRouter
+
 app = FastAPI()
 
 app.include_router(StudentRouter, tags=["Student"], prefix="/student")
-
+app.include_router(FunctionTestRouter, tags=["FunctionTest"], prefix="/functiontest")
 
 @app.get("/", tags=["Root"])
 async def read_root():
